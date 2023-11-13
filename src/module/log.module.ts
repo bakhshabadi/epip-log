f552ded988f82f734d4d6647241dfdd7e0097623
bakhshabadi <bakhshabadi.javad@gmail.com>
2023-11-13 10:03:30 +0330

    Update log.module.ts

--- src/module/log.module.ts
+++ src/module/log.module.ts
@@ -5,40 +5,41 @@ import { LogEntity, LogSchema } from '../models/log.model';
 import { LogService } from '../services/log.service';
 
 export interface LogOption {
-    connection: string;
-    user: string;
-    pass: string;
-    dbName: string;
+  connection: string;
+  user: string;
+  pass: string;
+  dbName: string;
 }
 
 @Module({})
 export class LogModule {
-    static register(options: LogOption): DynamicModule {
-        return {
-            module: LogModule,
-            imports: [
-                MongooseModule.forRootAsync({
-                    imports: [ConfigModule],
-                    inject: [ConfigService],
+  static register(options: LogOption): DynamicModule {
+    return {
+      module: LogModule,
+      imports: [
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
 
-                    useFactory: (srv: ConfigService): MongooseModuleOptions => {
-                        const retVal = {
-                            uri: srv.get(options.connection),
-                            user: srv.get(options.user),
-                            pass: srv.get(options.pass),
-                            dbName: srv.get(options.dbName),
-                        }
-                        return retVal;
-                    },
-                }),
-                MongooseModule.forFeature([{ name: LogEntity.name, schema: LogSchema }])
-            ],
-            providers: [
-                LogService,
-            ],
-            exports: [
-                LogService,
-            ]
-        };
-    }
+          useFactory: (srv: ConfigService): MongooseModuleOptions => {
+            const retVal = {
+              uri: srv.get(options.connection),
+              user: srv.get(options.user),
+              pass: srv.get(options.pass),
+              dbName: srv.get(options.dbName),
+            };
+            console.log('***************************');
+            console.log(retVal);
+            console.log('***************************');
+            return retVal;
+          },
+        }),
+        MongooseModule.forFeature([
+          { name: LogEntity.name, schema: LogSchema },
+        ]),
+      ],
+      providers: [LogService],
+      exports: [LogService],
+    };
+  }
 }
